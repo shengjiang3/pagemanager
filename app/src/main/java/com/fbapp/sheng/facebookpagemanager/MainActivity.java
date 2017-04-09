4ce8cffc5db3611954cae266fe70530aae83a319
LAPTOP-HO0PDAKD\sheng <sheng.jiang3@gmail.com>
2017-04-09 12:56:21 -0700

    First commit; Working UI

---
+++ app/src/main/java/com/fbapp/sheng/facebookpagemanager/MainActivity.java
@@ -0,0 +1,156 @@
+package com.fbapp.sheng.facebookpagemanager;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.fbapp.sheng.facebookpagemanager.dummy.DummyContent;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements PostsFragment.OnListFragmentInteractionListener{
+    public static final String TAG = "MainActivity";
+    CallbackManager callbackManager;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            Class fragmentClass = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_posts:
+                    fragmentClass = PostsFragment.class;
+                case R.id.navigation_metrics:
+                    fragmentClass = PostsFragment.class;
+                case R.id.navigation_inbox:
+                    fragmentClass = PostsFragment.class;
+                case R.id.navigation_notifications:
+                    fragmentClass = PostsFragment.class;
+            }
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+            return true;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        fragmentClass = PostsFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        showFacebookLoginDialog();
+    }
+
+    private void showFacebookLoginDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Login to Facebook");
+        final LinearLayout linearView = new LinearLayout(MainActivity.this);
+        builder.setView(linearView);
+
+        final AlertDialog alertDialog = builder.create();
+        LayoutInflater inflater = alertDialog.getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.facebook_login_dialog, linearView);
+        alertDialog.show();
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) dialogLayout.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.v(TAG, loginResult.toString());
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                Log.v(TAG, response.toString());
+                                try {
+                                    String name = object.getString("name");
+                                    Log.v(TAG, "obtained name: "+name);
+                                }
+                                catch(JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                );
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id, name, link");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e(TAG, "Facebook login cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.e(TAG, "Facebook login error");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem dummyItem) {
+
+    }
+}
